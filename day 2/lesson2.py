bac964779934fb4a592bb772935a5338ce197d69
Sabsoona <sabamitichashvili555@gmail.com>
2022-09-21 02:44:11 +0300

    second commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,14 @@
+name="saba"    
+surname="mitichashvili"
+age=16
+age1="wlis"
+height="176 santimetri"
+is_learning_programming=True
+hates_programming=False
+
+
+print(name+" "+surname+" "+str(age)+" "+age1+"  "+height)
+
+
+
+
