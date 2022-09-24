065444b1310aa22b67d93079a6219b4fac05b973
Sabsoona <sabamitichashvili555@gmail.com>
2022-09-24 18:53:23 +0300

    first commit

---
+++ day 3/lesson4.py
@@ -0,0 +1,24 @@
+#name="saba"
+#print(len(name))  #len aris sigrdze anu length
+
+#my_sentence = """გამარჯობა ყველას ჩემი სახელია საბა,
+ #სასიამოვნოა თქვენი გაცნობა """
+#if 10>5:
+#      print(my_sentence)
+#full_name="saba mitichashvili"
+
+#print(full_name)
+
+#print(full_name[3])   #position da index aris sinonimebi 
+
+
+#full_name="saba mitichashvili"
+#print(full_name[2:7])   #არ არის მდე ანუ არ ჩაითვლება
+
+#full_name="saba mitichashvili"
+#print(full_name[2:7:2])   #yoveli meore
+
+full_name="saba mitichashvili"
+print(full_name[:5])
+print(full_name[5:])
+
