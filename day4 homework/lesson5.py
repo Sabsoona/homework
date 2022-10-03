0898299a481e3434d44710ccf5c740f8eeb58a67
Sabsoona <sabamitichashvili555@gmail.com>
2022-10-03 11:58:02 +0300

    fourth commit

---
+++ day4 homework/lesson5.py
@@ -0,0 +1,11 @@
+my_name = input("enter your name: ")
+my_surname = input("enter your surname: ")
+my_age = int(input("enter your age: "))
+
+print("my_name")
+print("my_surname")
+print("my_age")
+if my_age > 18:
+
+    
+     print("you are adoult")
